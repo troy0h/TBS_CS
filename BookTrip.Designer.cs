a8f594216a6f839fe6b9bcd2854424f4bcb58789
POPKIZZ52 <148454371+POPKIZZ52@users.noreply.github.com>
2024-03-01 14:40:53 +0000

    added buttons in book trips

--- BookTrip.Designer.cs
+++ BookTrip.Designer.cs
@@ -28,18 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            listBox1 = new ListBox();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(230, 498);
+            button1.Name = "button1";
+            button1.Size = new Size(84, 75);
+            button1.TabIndex = 0;
+            button1.Text = "Back";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(619, 498);
+            button2.Name = "button2";
+            button2.Size = new Size(84, 75);
+            button2.TabIndex = 1;
+            button2.Text = "Request taxi";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(572, 129);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(205, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(619, 327);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(84, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(239, 396);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(304, 23);
+            textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(239, 259);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(117, 23);
+            textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(374, 259);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(113, 23);
+            textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(619, 396);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(84, 23);
+            textBox6.TabIndex = 7;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(239, 327);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(304, 23);
+            textBox7.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(552, 335);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 15);
+            label1.TabIndex = 9;
+            label1.Text = "Post Code";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(552, 404);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 15);
+            label2.TabIndex = 10;
+            label2.Text = "Post Code";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(239, 300);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 15);
+            label3.TabIndex = 11;
+            label3.Text = "Pick Up Address";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(239, 378);
+            label4.Name = "label4";
+            label4.Size = new Size(98, 15);
+            label4.TabIndex = 12;
+            label4.Text = "Drop Off Address";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(239, 231);
+            label5.Name = "label5";
+            label5.Size = new Size(33, 15);
+            label5.TabIndex = 13;
+            label5.Text = "Time";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(374, 231);
+            label6.Name = "label6";
+            label6.Size = new Size(31, 15);
+            label6.TabIndex = 14;
+            label6.Text = "Date";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(498, 231);
+            label7.Name = "label7";
+            label7.Size = new Size(54, 15);
+            label7.TabIndex = 15;
+            label7.Text = "Taxi Type";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Items.AddRange(new object[] { "Car", "Van", "Electric Car" });
+            listBox1.Location = new Point(498, 263);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(205, 19);
+            listBox1.TabIndex = 16;
             // 
             // BookTrip
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1008, 729);
+            Controls.Add(listBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Name = "BookTrip";
             Text = "BookTrip";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private ListBox listBox1;
     }
 }
--- BookTrip.cs
+++ BookTrip.cs
@@ -16,5 +16,10 @@ namespace TBS_CS
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
